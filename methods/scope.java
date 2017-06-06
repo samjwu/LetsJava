d5cfb0b4528d2b63a4229929b091fbf33b1868cc
Sam Wu <sjwu@ualberta.ca>
2017-06-05 22:11:38 -0700

    method scope

---
+++ methods/scope.java
@@ -0,0 +1,19 @@
+package methods;
+
+public class scope {
+    public static void main(String[] args) {
+        String name = "Sam";
+
+        greet("Sammy");
+
+        greet(name);
+        
+        name = "Sami";
+        greet(name);
+    }
+    
+    public static void greet( String name ) {
+        System.out.printf("Hello, %s\n",name);
+        name="Tarang";
+    }
+}
