37bc280679248e8ec5bf438facf182e92352a0e2
Sam Wu <sjwu@ualberta.ca>
2017-06-28 21:57:04 -0700

    for loop

---
+++ forloop/basicforloop.java
@@ -0,0 +1,13 @@
+package forloop;
+
+public class basicforloop {
+    
+    public static void main(String[] args) {
+        int iterations = 10;
+        System.out.println("Counting up...");
+        for (int i = 0; i < iterations; i++) {
+            System.out.printf("%d\n", i + 1);
+        }
+    }
+
+}
