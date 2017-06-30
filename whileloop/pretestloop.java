af79a21a0587141965a3ced40d0a70e51c12bc24
Sam Wu <sjwu@ualberta.ca>
2017-06-29 21:19:14 -0700

    pre test while loop

---
+++ whileloop/pretestloop.java
@@ -0,0 +1,27 @@
+package whileloop;
+
+import java.util.Random;
+import java.util.Scanner;
+
+/** 
+* pre test loops are not guaranteed to execute at all
+* if the loop condition is false before evaluation, the loop will not run
+*/
+public class pretestloop {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        Random generator = new Random();
+        int number = generator.nextInt(10) + 1;
+        int guess = 0;
+        int numattempts = 0;
+        System.out.println("Guess a number between 1 and 10 inclusive.");
+        while (guess != number) {
+            System.out.printf("Number of guesses: %d\n", numattempts);
+            System.out.printf("Your Guess: ");
+            guess = input.nextInt();
+            numattempts++;
+        }
+        System.out.printf("You guessed it. The number was %d.\n", guess);
+    }
+}
