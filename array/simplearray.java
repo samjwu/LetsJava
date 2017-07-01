57bda2dc456498fd9b162a80cdd7f4a50cacd312
Sam Wu <sjwu@ualberta.ca>
2017-06-30 22:10:55 -0700

    simple array

---
+++ array/simplearray.java
@@ -0,0 +1,30 @@
+package array;
+
+public class simplearray {
+
+    /**
+     * Arrays contain variables (elements) of the same type
+     * Arrays are referenced by name
+     * Elements of an array are referenced by index
+     */        
+    public static void main(String[] args) {                        
+        int[] levels = new int[4];  //declare empty array of size 4
+        //assign values
+        levels[0] = 90;  
+        levels[1] = 80;
+        levels[2] = 70;
+        levels[3] = 50;
+        
+        //statement to declare/assign values to array
+        String[] classes = {"A", "B", "C", "D"};
+        
+        System.out.printf("#\tClass\tScore\n");
+        System.out.printf("-------------------------\n");
+        
+        //for loops for enumerating array
+        for (int i=0;i<levels.length;i++)
+        {
+            System.out.printf("%d\t%s\t%d\n", i, classes[i], levels[i]);
+        }        
+    }
+}
